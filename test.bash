40c6a8038366f39e6a44650dfa6b559782576e91
Ryousuke Ochiai <s22C1028FL@s.chibakoudai.jp>
2023-11-02 12:35:08 +0900

    Add a command

--- test.bash
+++ test.bash
@@ -9,9 +9,17 @@ ng () {
 
 res=0
 
-### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15.0 ] || ng ${LINENO}
-
-[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
-exit $res
+  [ "${out}" = 15 ] || ng ${LINENO}
+    　
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　
+  out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　
+[ "$res" = 0 ] && echo OK
+  exit $res
